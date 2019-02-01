88a01ebbbc5e698aaae1f7e2c0d844dc713b09d0
Marios Sofokleous <marios.sofokleous@yandex.com>
2019-02-01 02:16:05 +0200

    Comply to ES2015. Host Font Awesome. Create Gruntfile.js, load Grunt plugins such as purifycss, cssmin, uglify, critical, htmlmin and configure various optimization tasks. Create optimize.sh to run multiple Grunt tasks in sequence. Create .gitignore file.

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    // Remove unused CSS
+    purifycss: {
+      options: {},
+      target: {
+        src: ['src/index.html', 'src/js/*.js'],
+        css: ['src/css/*.css'],
+        dest: 'docs/css/purestyles.css'
+      },
+    },
+    
+    // Minify CSS (purestyles.css)
+    cssmin: {
+      t1: {
+        files: [{
+          expand: true,
+          cwd: 'docs/css/',
+          src: ['purestyles.css'],
+          dest: 'docs/css/',
+          ext: '.min.css'
+        }]
+      }
+    },
+    
+    // Minify JavaScript (site.js)
+    uglify: {
+      t1: {
+        files: [{
+          expand: true,
+          cwd: 'src/js/',
+          src: ['*.js', '!*.min.js'],
+          dest: 'docs/js',
+          ext: '.min.js'
+        }]
+      }
+    },
+    
+    // Copy index.html to docs.
+    // Copy all the minified javascript files to docs/js.
+    copy: {
+      t1: {
+        expand: true,
+        cwd: 'src/',
+        src: 'index.html',
+        dest: 'docs/'
+      },
+      t2: {
+        expand: true,
+        cwd: 'src/js/',
+        src: '*.min.js',
+        dest: 'docs/js/'
+      }
+    },
+    
+    // Extract & inline critical-path CSS from docs/index.html
+    critical: {
+      t1: {
+        options: {
+          base: 'docs/',
+          css: [
+            'docs/css/purestyles.min.css'
+          ],
+          width: 1920,
+          height: 1080
+        },
+        src: 'docs/index.html',
+        dest: 'docs/index.html'
+      }
+    },
+    
+    // Minify docs/index.html
+    htmlmin: {                                    
+      t1: {                                     
+        options: {                                 
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'docs/',
+          src: 'index.html',
+          dest: 'docs/'
+        }]
+      }
+    }
+
+  });
+
+  // Load the grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-purifycss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-critical');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+  // Default task(s).
+  //grunt.registerTask('default', ['clean']);
+  //grunt.registerTask('default', ['purifycss']);
+  //grunt.registerTask('default', ['cssmin']);
+  //grunt.registerTask('default', ['critical']);
+  //grunt.registerTask('default', ['htmlmin']);
+};

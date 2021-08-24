6f0eda6c655ce97214eeac14c74b5f6862fdf267
PictureElement <marios.sofokleous@yandex.com>
2021-08-24 23:15:19 +0300

    Clean project

--- gulpfile.js
+++ gulpfile.js
@@ -16,60 +16,50 @@ const gulpif = require('gulp-if');
 
 // Clean dist
 function cleanDist() {
-  return del(['dist/**', '!dist']);
+    return del(['dist/**', '!dist']);
 }
 
 // Clean vendor (src)
 function cleanVendor() {
-  return del(['src/vendor/**', '!src/vendor']);
+    return del(['src/vendor/**', '!src/vendor']);
 }
 
 // Populate vendor (src)
 function populateVendor() {
-  return gulp.src([
-    'node_modules/materialize-css/dist/js/materialize.min.js',
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/echarts/dist/echarts.min.js',
-    'node_modules/materialize-css/sass/components/**/*',
-    'node_modules/@fortawesome/fontawesome-free/css/all.min.css',
-    'node_modules/@fortawesome/fontawesome-free/webfonts/**/*',
-    'node_modules/simplebar/dist/simplebar.min.js',
-    'node_modules/simplebar/dist/simplebar.min.css',
-    'node_modules/aos/dist/aos.js',
-    'node_modules/aos/dist/aos.css',
-    'node_modules/headroom.js/dist/headroom.min.js'
-  ], {base: 'node_modules/'})
-  .pipe(gulp.dest('src/vendor/'));
-}
-
-// Copy icons to dist
-function copyIcons() {
-  return gulp.src('src/icons/**/*')
-    .pipe(gulp.dest('dist/icons'));
+    return gulp.src([
+            'node_modules/materialize-css/dist/js/materialize.min.js',
+            'node_modules/materialize-css/sass/components/**/*',
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/echarts/dist/echarts.min.js',
+            'node_modules/@fortawesome/fontawesome-free/css/all.min.css',
+            'node_modules/@fortawesome/fontawesome-free/webfonts/**/*',
+            'node_modules/simplebar/dist/simplebar.min.js',
+            'node_modules/simplebar/dist/simplebar.min.css',
+            'node_modules/aos/dist/aos.js',
+            'node_modules/aos/dist/aos.css',
+            'node_modules/headroom.js/dist/headroom.min.js'
+        ], {
+            base: 'node_modules/'
+        })
+        .pipe(gulp.dest('src/vendor/'));
 }
 
 // Copy images to dist
 function copyImages() {
-  return gulp.src('src/images/**/*')
-    .pipe(gulp.dest('dist/images'));
-}
-
-// Copy theme to dist
-function copyTheme() {
-  return gulp.src('src/theme/**/*')
-    .pipe(gulp.dest('dist/theme'));
+    return gulp.src('src/images/**/*')
+        .pipe(gulp.dest('dist/images'));
 }
 
 // Copy vendor to dist
 function copyVendor() {
-  return gulp.src('src/vendor/**/*')
-    .pipe(gulp.dest('dist/vendor'));
+    return gulp.src('src/vendor/**/*')
+        .pipe(gulp.dest('dist/vendor'));
 }
 
 // Copy Html to dist
 function copyHtml() {
-  return gulp.src('src/*.html')
-    .pipe(gulp.dest('dist'));
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
 }
 
 // Development
@@ -77,34 +67,34 @@ function copyHtml() {
 
 // Static server (src)
 function browserSyncSrc(cb) {
-  browsersync.init({
-    server: {
-      baseDir: "src"
-    },
-    open: "external"
-  });
-  cb();
+    browsersync.init({
+        server: {
+            baseDir: "src"
+        },
+        open: "external"
+    });
+    cb();
 }
 
 // Browser reload
 function browserReload(cb) {
-  browsersync.reload();
-  cb(); // Signal completion
+    browsersync.reload();
+    cb(); // Signal completion
 }
 
 // Compile Sass files
 function compileSass() {
-  return gulp.src('src/sass/developerportfolio.scss')
-    .pipe(sass().on('error', sass.logError)) // Compile to CSS
-    .pipe(gulp.dest('src/css/')) // Save to src
-    .pipe(browsersync.stream()); // Inject changes without refreshing the page.
+    return gulp.src('src/sass/developerportfolio.scss')
+        .pipe(sass().on('error', sass.logError)) // Compile to CSS
+        .pipe(gulp.dest('src/css/')) // Save to src
+        .pipe(browsersync.stream()); // Inject changes without refreshing the page.
 }
 
 // Watch scss/js/html/ files
 function watchFiles() {
-  gulp.watch("src/sass/**/*.scss", gulp.series(compileSass));
-  gulp.watch(["src/js/**/*.js"], gulp.series(browserReload));
-  gulp.watch("src/*.html", gulp.series(browserReload));
+    gulp.watch("src/sass/**/*.scss", gulp.series(compileSass));
+    gulp.watch(["src/js/**/*.js"], gulp.series(browserReload));
+    gulp.watch("src/*.html", gulp.series(browserReload));
 }
 
 // Production
@@ -112,73 +102,77 @@ function watchFiles() {
 
 // CSS optimization
 function css() {
-  return gulp.src('src/css/*.css')
-  .pipe(autoprefixer({cascade: false})) // Add vendor prefixes
-  .pipe(cleanCSS({compatibility: 'ie8'})) // Minify CSS
-  .pipe(gulp.dest('dist/css/'));
+    return gulp.src('src/css/*.css')
+        .pipe(autoprefixer({
+            cascade: false
+        })) // Add vendor prefixes
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        })) // Minify CSS
+        .pipe(gulp.dest('dist/css/'));
 }
 
 // JS optimization
 function js() {
-  return gulp.src('src/js/*.js')
-    .pipe(uglify()) // Minify JS
-    .pipe(gulp.dest('dist/js/'));
+    return gulp.src('src/js/*.js')
+        .pipe(uglify()) // Minify JS
+        .pipe(gulp.dest('dist/js/'));
 }
 
 // Critical CSS
 function criticalCSS() {
-  return gulp.src('dist/*.html')
-    .pipe(
-      critical({
-          inline: true,
-          base: 'dist/',
-          // ignore: ['@font-face'],
-          dimensions: [
-            {
-              // 9:16 (Mobile)
-              height: 1022,
-              width: 575,
-            },
-            {
-              // 9:16 (Tablet)
-              height: 1364,
-              width: 767,
-            },
-            {
-              // 9:16 (Tablet)
-              height: 1762,
-              width: 991,
-            },
-            {
-              // 9:16 (Tablet)
-              height: 2132,
-              width: 1199,
-            },
-            {
-              // 9:16 (Tablet)
-              height: 2132,
-              width: 1199,
-            },
-            {
-              // 3:2 (Desktop)
-              height: 800,
-              width: 1200,
-            },
-          ]
-      })
-    )
-    .on('error', function(err) { 
-      document(error);
-    })
-    .pipe(gulp.dest('dist/'));
+    return gulp.src('dist/*.html')
+        .pipe(
+            critical({
+                inline: true,
+                base: 'dist/',
+                // ignore: ['@font-face'],
+                dimensions: [{
+                        // 9:16 (Mobile)
+                        height: 1022,
+                        width: 575,
+                    },
+                    {
+                        // 9:16 (Tablet)
+                        height: 1364,
+                        width: 767,
+                    },
+                    {
+                        // 9:16 (Tablet)
+                        height: 1762,
+                        width: 991,
+                    },
+                    {
+                        // 9:16 (Tablet)
+                        height: 2132,
+                        width: 1199,
+                    },
+                    {
+                        // 9:16 (Tablet)
+                        height: 2132,
+                        width: 1199,
+                    },
+                    {
+                        // 3:2 (Desktop)
+                        height: 800,
+                        width: 1200,
+                    },
+                ]
+            })
+        )
+        .on('error', function (err) {
+            document(error);
+        })
+        .pipe(gulp.dest('dist/'));
 }
 
 // Tasks
 // =============================================================================
 
 // Define tasks
-const init = gulp.series(cleanDist, cleanVendor, populateVendor, copyIcons, copyImages, copyTheme, copyVendor, copyHtml);
-const build = gulp.series(init, compileSass, css, js, criticalCSS);
+const init = gulp.series(cleanDist, cleanVendor, populateVendor, copyImages, copyTheme, copyVendor, copyHtml);
+// const build = gulp.series(init, compileSass, css, js, criticalCSS);
+const build = gulp.series(init, compileSass, css, js);
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSyncSrc));
 
 // Register public tasks
--- src/js/developerportfolio.js
+++ src/js/developerportfolio.js
@@ -1,88 +1,92 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  // Make top bar react to user's scroll
-  var topBar = document.querySelector(".top-bar");
-  var headroom  = new Headroom(topBar);
-  headroom.init();
+    // Make top bar react to user's scroll
+    var topBar = document.querySelector(".top-bar");
+    var headroom = new Headroom(topBar);
+    headroom.init();
 
-  // Parallax
-  var parallax = document.getElementById('parallax');
-  M.Parallax.init(parallax);
+    // Parallax
+    var parallax = document.getElementById('parallax');
+    M.Parallax.init(parallax);
 
-  // Init AOS
-  AOS.init();
+    // Init AOS
+    AOS.init();
 
-  // Chart
-  var dom = document.getElementById('skillsChart');
-  var skillsChart = echarts.init(dom, 'light', {renderer: 'canvas'});
-  skillsChart.showLoading();
-  var data;
-  var option;
+    // Chart
+    var dom = document.getElementById('skillsChart');
+    var skillsChart = echarts.init(dom, 'light', {
+        renderer: 'canvas'
+    });
+    skillsChart.showLoading();
+    var data;
+    var option;
 
-  function status(response) {
-    if (response.status >= 200 && response.status < 300) {
-      return Promise.resolve(response);
-    } else {
-      return Promise.reject(new Error(response.statusText));
+    function status(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return Promise.resolve(response);
+        } else {
+            return Promise.reject(new Error(response.statusText));
+        }
     }
-  }
-  
-  function json(response) {
-    return response.json();
-  }
 
-  fetch('js/config/data.json')
-    .then(status)
-    .then(json)
-    .then(function(response) {
-      console.log('Request succeeded with JSON response', response);
-      data = response;
-      return fetch('js/config/option.json');
-    })
-    .then(status)
-    .then(json)
-    .then(function(response) {
-      option = response;
-      option.baseOption.series.data = data;
-      console.log('Request succeeded with JSON response', response);
-      skillsChart.hideLoading();
-      skillsChart.setOption(option, true);
-      // Resize chart, when window size changes
-      window.onresize = function() {
-        skillsChart.resize();
-      };
-    })
-    .catch(function(error) {
-      console.log('Request failed', error);
+    function json(response) {
+        return response.json();
+    }
+
+    fetch('js/config/data.json')
+        .then(status)
+        .then(json)
+        .then(function (response) {
+            console.log('Request succeeded with JSON response', response);
+            data = response;
+            return fetch('js/config/option.json');
+        })
+        .then(status)
+        .then(json)
+        .then(function (response) {
+            option = response;
+            option.baseOption.series.data = data;
+            console.log('Request succeeded with JSON response', response);
+            skillsChart.hideLoading();
+            skillsChart.setOption(option, true);
+            // Resize chart, when window size changes
+            window.onresize = function () {
+                skillsChart.resize();
+            };
+        })
+        .catch(function (error) {
+            console.log('Request failed', error);
+        });
+
+    // Sidenav
+    var sidenav = document.getElementById('sidenav');
+    M.Sidenav.init(sidenav);
+
+    // Show more/less projects
+    $('#showMoreProjects').click(function () {
+        if ($('.projects-section__row-wrapper').hasClass('collapse')) {
+            $('.projects-section__row-wrapper').removeClass('collapse');
+            $(this).html('<i class="fas fa-chevron-up"></i> Show less');
+        } else {
+            $('.projects-section__row-wrapper').addClass('collapse');
+            $(this).html('<i class="fas fa-chevron-down"></i> Show more');
+        }
     });
 
-  // Sidenav
-  var sidenav = document.getElementById('sidenav');
-  M.Sidenav.init(sidenav);
-    
-  // Show more/less projects
-  $('#showMoreProjects').click(function () {
-    if ($('.projects-section__row-wrapper').hasClass('collapse')) {
-      $('.projects-section__row-wrapper').removeClass('collapse');
-      $(this).html('<i class="fas fa-chevron-up"></i> Show less');
-    } else {
-      $('.projects-section__row-wrapper').addClass('collapse');
-      $(this).html('<i class="fas fa-chevron-down"></i> Show more');
-    }
-  });
+    // Show more/less experience
+    $('#showMoreExperience').click(function () {
+        if ($('.timeline').hasClass('collapse')) {
+            $('.timeline').removeClass('collapse');
+            $(this).html('<i class="fas fa-chevron-up"></i> Show less');
+        } else {
+            $('.timeline').addClass('collapse');
+            $(this).html('<i class="fas fa-chevron-down"></i> Show more');
+        }
+    });
 
-  // Show more/less experience
-  $('#showMoreExperience').click(function () {
-    if ($('.timeline').hasClass('collapse')) {
-      $('.timeline').removeClass('collapse');
-      $(this).html('<i class="fas fa-chevron-up"></i> Show less');
-    } else {
-      $('.timeline').addClass('collapse');
-      $(this).html('<i class="fas fa-chevron-down"></i> Show more');
-    }
-  });
-
-  // Scrollspy
-  var elems = document.querySelectorAll('.scrollspy');
-  M.ScrollSpy.init(elems, {scrollOffset: 0});
+    // Scrollspy
+    var elems = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(elems, {
+        scrollOffset: 0
+    });
 });

67d0b5ce7a3ba549aad375b8ee20639726f638a8
PictureElement <marios.sofokleous@yandex.com>
2021-08-24 22:35:40 +0300

    Add testimonials section. Organize sass in multiple files.

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,25 @@ const del = require('del');
 const browsersync = require('browser-sync').create();
 const uglify = require('gulp-uglify');
 const wait = require('gulp-wait');
+const critical = require('critical').stream;
+const useref = require('gulp-useref');
+const gulpif = require('gulp-if');
 
-// Static server
-function browserSync(cb) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    },
-    open: "external"
-  });
-  cb();
+// Initialize
+// =============================================================================
+
+// Clean dist
+function cleanDist() {
+  return del(['dist/**', '!dist']);
 }
 
-// Browser reload
-function browserReload(cb) {
-  browsersync.reload();
-  cb(); // Signal completion
+// Clean vendor (src)
+function cleanVendor() {
+  return del(['src/vendor/**', '!src/vendor']);
 }
 
-// Clean vendor directory
-function clean() {
-  return del(['vendor/**', '!vendor']);
-}
-
-// Copy third-party modules in vendor directory
-function copy() {
+// Populate vendor (src)
+function populateVendor() {
   return gulp.src([
     'node_modules/materialize-css/dist/js/materialize.min.js',
     'node_modules/jquery/dist/jquery.min.js',
@@ -45,41 +39,149 @@ function copy() {
     'node_modules/aos/dist/aos.css',
     'node_modules/headroom.js/dist/headroom.min.js'
   ], {base: 'node_modules/'})
-  .pipe(gulp.dest('vendor/'));
+  .pipe(gulp.dest('src/vendor/'));
 }
 
-// Compile Sass file, minify CSS file and auto-inject into browser.
-function css() {
-  return gulp.src('sass/*.scss')
-    .pipe(sass())
-    .pipe(autoprefixer({cascade: false}))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css/'))
+// Copy icons to dist
+function copyIcons() {
+  return gulp.src('src/icons/**/*')
+    .pipe(gulp.dest('dist/icons'));
+}
+
+// Copy images to dist
+function copyImages() {
+  return gulp.src('src/images/**/*')
+    .pipe(gulp.dest('dist/images'));
+}
+
+// Copy theme to dist
+function copyTheme() {
+  return gulp.src('src/theme/**/*')
+    .pipe(gulp.dest('dist/theme'));
+}
+
+// Copy vendor to dist
+function copyVendor() {
+  return gulp.src('src/vendor/**/*')
+    .pipe(gulp.dest('dist/vendor'));
+}
+
+// Copy Html to dist
+function copyHtml() {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+}
+
+// Development
+// =============================================================================
+
+// Static server (src)
+function browserSyncSrc(cb) {
+  browsersync.init({
+    server: {
+      baseDir: "src"
+    },
+    open: "external"
+  });
+  cb();
+}
+
+// Browser reload
+function browserReload(cb) {
+  browsersync.reload();
+  cb(); // Signal completion
+}
+
+// Compile Sass files
+function compileSass() {
+  return gulp.src('src/sass/developerportfolio.scss')
+    .pipe(sass().on('error', sass.logError)) // Compile to CSS
+    .pipe(gulp.dest('src/css/')) // Save to src
     .pipe(browsersync.stream()); // Inject changes without refreshing the page.
-}
-
-// Minify JavaScript file and auto-inject into browser.
-function js() {
-  return gulp.src(['js/*.js', '!js/*.min.js'])
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('js/'));
 }
 
 // Watch scss/js/html/ files
 function watchFiles() {
-  gulp.watch("sass/*.scss", gulp.series(css));
-  gulp.watch(["js/*.js", "!js/*.min.js"], gulp.series(js, browserReload));
-  gulp.watch("*.html", gulp.series(browserReload));
+  gulp.watch("src/sass/**/*.scss", gulp.series(compileSass));
+  gulp.watch(["src/js/**/*.js"], gulp.series(browserReload));
+  gulp.watch("src/*.html", gulp.series(browserReload));
 }
 
-// Define complex tasks
-const vendor = gulp.series(clean, copy);
-const build = gulp.series(vendor, gulp.parallel(css, js));
-const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+// Production
+// =============================================================================
+
+// CSS optimization
+function css() {
+  return gulp.src('src/css/*.css')
+  .pipe(autoprefixer({cascade: false})) // Add vendor prefixes
+  .pipe(cleanCSS({compatibility: 'ie8'})) // Minify CSS
+  .pipe(gulp.dest('dist/css/'));
+}
+
+// JS optimization
+function js() {
+  return gulp.src('src/js/*.js')
+    .pipe(uglify()) // Minify JS
+    .pipe(gulp.dest('dist/js/'));
+}
+
+// Critical CSS
+function criticalCSS() {
+  return gulp.src('dist/*.html')
+    .pipe(
+      critical({
+          inline: true,
+          base: 'dist/',
+          // ignore: ['@font-face'],
+          dimensions: [
+            {
+              // 9:16 (Mobile)
+              height: 1022,
+              width: 575,
+            },
+            {
+              // 9:16 (Tablet)
+              height: 1364,
+              width: 767,
+            },
+            {
+              // 9:16 (Tablet)
+              height: 1762,
+              width: 991,
+            },
+            {
+              // 9:16 (Tablet)
+              height: 2132,
+              width: 1199,
+            },
+            {
+              // 9:16 (Tablet)
+              height: 2132,
+              width: 1199,
+            },
+            {
+              // 3:2 (Desktop)
+              height: 800,
+              width: 1200,
+            },
+          ]
+      })
+    )
+    .on('error', function(err) { 
+      document(error);
+    })
+    .pipe(gulp.dest('dist/'));
+}
+
+// Tasks
+// =============================================================================
+
+// Define tasks
+const init = gulp.series(cleanDist, cleanVendor, populateVendor, copyIcons, copyImages, copyTheme, copyVendor, copyHtml);
+const build = gulp.series(init, compileSass, css, js, criticalCSS);
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSyncSrc));
 
 // Register public tasks
-exports.vendor = vendor;
+exports.init = init;
 exports.build = build;
 exports.watch = watch;
